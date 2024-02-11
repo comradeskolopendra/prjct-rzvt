77beb724f755be175a923bad5ca6d25951f55bf4
comradeskolopendra <comradeskolopendra@gmail.com>
2024-02-11 13:16:30 +0600

    Добавить краткую информацию о проекте на главную страницу

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,4 +1,45 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const AnimateSentence = keyframes`
+  0% {
+    width: 0%;
+  }
+
+  25% {
+    width: 100%;
+  }
+
+  50% {
+    width: 100%;
+  }
+
+  80% {
+    width: 0%;
+  }
+
+  100% {
+    width: 0%;
+  }
+`
+
+const SelectSentence = styled.span`
+  font-style: italic;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 1px;
+    background: white;
+    animation-name: ${AnimateSentence};
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-delay: 1s;
+  }
+`
 
 const TitleMain = styled.h1`
   color: white;
@@ -11,15 +52,26 @@ const TitleMain = styled.h1`
 
 const SubtitleMain = styled.p`
   color: white;
-  font-size: 24px;
+  font-size: 26px;
   font-weight: 500;
   font-family: "Open Sans", sans-serif;
   max-width: 540px;
   margin-top: 0;
+  margin-bottom: 15px;
 `
 
 const SubtitleAbout = styled(SubtitleMain)`
-  font-weight: 300
+  font-weight: 300;
+  font-size: 20px;
+  opacity: 0.75;
+  transition: opacity 0.3s ease-in-out;
+  max-width: 510px;
+  margin-bottom: 0;
+
+  &:hover {
+    opacity: 1;
+    transition: opacity 0.3s ease-in-out;
+  }
 `
 
 const MainPage = () => {
@@ -30,15 +82,10 @@ const MainPage = () => {
         Сбер.Соц
       </TitleMain>
       <SubtitleMain>
-        <span style={{
-          marginBottom: "15px",
-          fontWeight: "500"
-        }}>
-          Поделись знаниями с ближним своим!
-        </span>
+        Поделись знаниями с ближним своим!
       </SubtitleMain>
       <SubtitleAbout>
-        Здесь Вы можете размещать образовательные ресурсы, видеоролики, делится анализом и многое другое
+        Здесь Вы можете <SelectSentence>делится</SelectSentence> образовательными ресурсами, видеороликами, анализомами и многим другим
       </SubtitleAbout>
     </section >
   )
