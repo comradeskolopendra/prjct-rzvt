476ed525db44b0820cf6d2550b08367ec2e3cc8a
just-sain <just.sain29@gmail.com>
2024-02-11 14:30:29 +0600

    Добавить сервисы для авторизаций

--- src/services/gql.ts
+++ src/services/gql.ts
@@ -1,5 +1,27 @@
-export const getThemesGql = `query {
-	themes{
+export const getThemesGql = ({
+	page = 1,
+	pageSize = 5,
+	isConfirm = true,
+}: {
+	page: number;
+	pageSize: number;
+	isConfirm: boolean;
+}) => {
+	return `query {
+	themes(
+		pagination: {
+			page: ${page}
+			pageSize: ${pageSize}
+		}
+		${
+			isConfirm &&
+			`filters: {
+				isConfirmed: {
+					eq: true
+				}
+			}`
+		}
+	){
 		data {
 			id
 			attributes {
@@ -35,3 +57,43 @@ export const getThemesGql = `query {
 		}
 	}
 }`;
+};
+
+/*
+ * auth
+ * block
+ */
+
+// login
+export const loginGql = ({ identifier, password }: { identifier: string; password: string }) => {
+	return `mutation {
+		login(input: { identifier: "${identifier}", password: "${password}" }) {
+			jwt
+			user {
+				id
+				username
+			}
+		}
+	}`;
+};
+
+// register
+export const registerGql = ({
+	username,
+	email,
+	password,
+}: {
+	username: string;
+	email: string;
+	password: string;
+}) => {
+	return `mutation {
+		register(input: { username: "${username}", email: "${email}", password: "${password}" }) {
+			jwt
+			user {
+				username
+				email
+			}
+		}
+	}`;
+};
--- src/services/strapi.ts
+++ src/services/strapi.ts
@@ -1,19 +1,53 @@
 import axios from 'axios';
 import { GetThemes } from '.';
-import { getThemesGql } from './gql';
+import { getThemesGql, loginGql, registerGql } from './gql';
 
 class Strapi {
 	private URL = import.meta.env.VITE_STRAPI_API;
 	private GRAPHQL = import.meta.env.VITE_STRAPI_GRAPHQL;
 	private TOKEN = import.meta.env.VITE_STRAPI_DEV_TOKEN;
 
-	async getThemes() {
-		return axios.post<GetThemes>(`${this.GRAPHQL}`, {
-			data: getThemesGql,
+	async getThemes({
+		page = 1,
+		pageSize = 10,
+		isConfirm = true,
+	}: {
+		pageSize?: number;
+		page?: number;
+		isConfirm?: boolean;
+	}) {
+		const { data } = await axios.post<GetThemes>(`${this.GRAPHQL}`, {
+			query: getThemesGql({ page, pageSize, isConfirm }),
 			Headers: {
 				Authorization: `Bearer ${this.TOKEN}`,
 			},
 		});
+
+		return data;
+	}
+
+	async login({ identifier, password }: { identifier: string; password: string }) {
+		const { data } = await axios.post<GetThemes>(`${this.GRAPHQL}`, {
+			query: loginGql({ identifier, password }),
+		});
+
+		return data;
+	}
+
+	async register({
+		username,
+		email,
+		password,
+	}: {
+		username: string;
+		email: string;
+		password: string;
+	}) {
+		const { data } = await axios.post<GetThemes>(`${this.GRAPHQL}`, {
+			query: registerGql({ username, email, password }),
+		});
+
+		return data;
 	}
 }
 
