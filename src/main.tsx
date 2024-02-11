2e3109d11724323b07e5ffca7b85f352496471b4
comradeskolopendra <comradeskolopendra@gmail.com>
2024-02-11 10:11:02 +0600

    Добавить скрипты на деплой

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {Provider} from "react-redux";
 
 import App from './App.tsx'
 import './index.css'
@@ -11,9 +10,9 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={client}>
-        <Provider>
-          <App/>
-        </Provider>
+        {/*<Provider>*/}
+        <App/>
+        {/*</Provider>*/}
       </QueryClientProvider>
     </React.StrictMode>,
 )
