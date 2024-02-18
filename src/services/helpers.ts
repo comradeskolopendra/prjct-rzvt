478c9b13d736a45bf44ccace94df9d1e00900db7
comradeskolopendra <comradeskolopendra@gmail.com>
2024-02-18 22:32:13 +0600

    Добавить функцию проверки запроса

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -2,12 +2,22 @@ const formatMaxChars = (paragraph: string) => {
     return paragraph.slice(0, 127).concat("...");
 };
 
-const request = (url: string, options: object = {}) => {
-    return fetch(url, options);
+const request = async (url: string, options: object = {}): Promise<Response> => {
+    return await fetch(url, options).then(checkResponse);
 };
 
-// const requestWithRefresh = () => { }
+const checkResponse = (response: Response): any => {
+    try {
+        if (response.ok) {
+            response.json();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export {
-    formatMaxChars
+    formatMaxChars,
+    request,
+    checkResponse
 }
